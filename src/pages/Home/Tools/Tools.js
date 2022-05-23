bdf3cdd387bcb36563613ac78a286e276a53ab30
Sakib Rahman <seu.sakib@gmail.com>
2022-05-23 15:22:07 +0600

    fetch data using axios

--- src/pages/Home/Tools/Tools.js
+++ src/pages/Home/Tools/Tools.js
@@ -1,8 +1,13 @@
+import axios from "axios";
 import React from "react";
+import { useQuery } from "react-query";
 import ToolsCard from "./ToolsCard";
 
 const Tools = () => {
-  //   console.log(data);
+  const { data } = useQuery("allTools", () =>
+    axios.get("http://localhost:5000/tools")
+  );
+
   return (
     <div className=" mt-16">
       <h4 className="upper text-center text-lg text-red-700 font-semibold my-4">
@@ -11,7 +16,11 @@ const Tools = () => {
       <h2 className="text-4xl text-center font-medium">
         Do Your Work <span className="font-bold">Perfectly & Smoothly</span>
       </h2>
-      <ToolsCard />
+      <div className="grid grid-cols-3 justify-items-center mt-16 ">
+        {tools.map((tool) => (
+          <ToolsCard key={tool._id} tool={tool} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Home/Tools/ToolsCard.js
+++ src/pages/Home/Tools/ToolsCard.js
@@ -3,26 +3,24 @@ import React from "react";
 const ToolsCard = ({ tool }) => {
   const { img, name, desc, min_order, available, price_unit } = tool;
   return (
-    <div className="grid grid-cols-3 justify-items-center mt-16 ">
-      <div class="card card-compact w-96 bg-white shadow-xl ">
-        <figure>
-          <img src={img} alt="Shoes" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">{name}</h2>
-          <p>{desc.slice(0, 150)}...</p>
-          <p>
-            <span className="font-bold">Minimum Order:</span> {min_order}
-          </p>
-          <p>
-            <span className="font-bold">Abailable:</span> {available}
-          </p>
-          <p>
-            <span className="font-bold">Price/unit:</span> ${price_unit}
-          </p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-block btn-primary ">Buy Now</button>
-          </div>
+    <div class="card card-compact w-96 bg-white shadow-xl ">
+      <figure>
+        <img src={img} alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">{name}</h2>
+        <p>{desc.slice(0, 150)}...</p>
+        <p>
+          <span className="font-bold">Minimum Order:</span> {min_order}
+        </p>
+        <p>
+          <span className="font-bold">Abailable:</span> {available}
+        </p>
+        <p>
+          <span className="font-bold">Price/unit:</span> ${price_unit}
+        </p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-block btn-primary ">Buy Now</button>
         </div>
       </div>
     </div>
