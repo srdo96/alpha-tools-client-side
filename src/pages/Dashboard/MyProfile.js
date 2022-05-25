2b0fc2a701bf6a76ad8b85c3d2f51b4613b04285
Sakib Rahman <seu.sakib@gmail.com>
2022-05-25 19:23:07 +0600

    fetch user data

--- src/pages/Dashboard/MyProfile.js
+++ src/pages/Dashboard/MyProfile.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import toast from "react-hot-toast";
+import { useQuery } from "react-query";
+import fetcher from "../../api/axiosInstance";
 import Loading from "../../components/Loading/Loading";
 import auth from "../../firebase.init";
 
@@ -11,13 +14,30 @@ const MyProfile = () => {
   const [editLinkedIn, setEditLinkedIn] = useState(false);
   const [update, setUpdata] = useState("");
 
-  if (loading) {
+  const { data, refetch, isLoading } = useQuery("user", () =>
+    fetcher(`user/${email}`)
+  );
+
+  if (loading || isLoading) {
     return (
       <div className="h-screen flex justify-center ">
         <Loading />;
       </div>
     );
   }
+
+  const { education, location, phone, linkedIn } = data?.data;
+
+  const handleUpdate = (name) => {
+    console.log("e", name);
+    const data = {
+      [name]: update,
+    };
+    fetcher.put(`user/${email}`, data).then((res) => {
+      toast.success("Update Successful");
+      refetch();
+    });
+  };
 
   return (
     <div className="bg-white shadow overflow-hidden sm:rounded-lg h-screen">
@@ -34,34 +54,44 @@ const MyProfile = () => {
 
       <div className="border-t border-gray-200">
         <dl>
+          {/* Name */}
           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">Full name</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
               {displayName}
             </dd>
           </div>
+
+          {/* email */}
           <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">Email Address</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 ">
               {email}
             </dd>
           </div>
+
+          {/* Education */}
           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">Education</dt>
             {editEducation ? (
               <dd className="lg:flex lg:gap-2">
                 <input
                   type="text"
-                  id="simple-email"
+                  onChange={(e) => setUpdata(e.target.value)}
                   class="   border border-t-0 border-x-0 border-gray-300 w-full pt-px px-4  bg-gray-50 text-gray-700 placeholder-gray-400  text-base focus:outline-none  "
                   placeholder="Your Education"
                 />
                 <span
-                  href="#"
+                  onClick={(e) => {
+                    handleUpdate(e.target.id);
+                    setEditEducation(false);
+                  }}
+                  id="education"
                   className="font-medium text-sm text-indigo-600 hover:text-indigo-500 cursor-pointer"
                 >
                   Update
                 </span>
+
                 <span className="mx-2">/</span>
                 <span
                   onClick={() => setEditEducation(false)}
@@ -72,7 +102,7 @@ const MyProfile = () => {
               </dd>
             ) : (
               <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex justify-between">
-                BSc in CSE
+                <p>{education}</p>
                 <span
                   onClick={() => setEditEducation(true)}
                   className="font-medium text-indigo-600 hover:text-indigo-500 mr-32 cursor-pointer ml-5"
@@ -82,18 +112,24 @@ const MyProfile = () => {
               </dd>
             )}
           </div>
+
+          {/* Location */}
           <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">Location</dt>
             {editLocation ? (
               <dd className="lg:flex lg:gap-2">
                 <input
+                  onChange={(e) => setUpdata(e.target.value)}
                   type="text"
-                  id="simple-email"
                   class="   border border-t-0 border-x-0 border-gray-300 w-full pt-px px-4  bg-white text-gray-700 placeholder-gray-400  text-base focus:outline-none  "
                   placeholder="Your Location"
                 />
                 <span
-                  href="#"
+                  onClick={(e) => {
+                    handleUpdate(e.target.id);
+                    setEditLocation(false);
+                  }}
+                  id="location"
                   className="font-medium text-sm text-indigo-600 hover:text-indigo-500 cursor-pointer"
                 >
                   Update
@@ -108,7 +144,7 @@ const MyProfile = () => {
               </dd>
             ) : (
               <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex justify-between">
-                Dhaka
+                {location}
                 <span
                   onClick={() => setEditLocation(true)}
                   className="font-medium text-indigo-600 hover:text-indigo-500 mr-32 cursor-pointer ml-5"
@@ -118,18 +154,24 @@ const MyProfile = () => {
               </dd>
             )}
           </div>
+
+          {/* Phone */}
           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">Phone Number</dt>
             {editPhone ? (
               <dd className="lg:flex lg:gap-2">
                 <input
+                  onChange={(e) => setUpdata(e.target.value)}
                   type="text"
-                  id="simple-email"
                   class="   border border-t-0 border-x-0 border-gray-300 w-full pt-px px-4  bg-gray-50 text-gray-700 placeholder-gray-400  text-base focus:outline-none  "
                   placeholder="Your Phone Number"
                 />
                 <span
-                  href="#"
+                  onClick={(e) => {
+                    handleUpdate(e.target.id);
+                    setEditPhone(false);
+                  }}
+                  id="phone"
                   className="font-medium text-sm text-indigo-600 hover:text-indigo-500 cursor-pointer"
                 >
                   Update
@@ -144,7 +186,7 @@ const MyProfile = () => {
               </dd>
             ) : (
               <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex justify-between">
-                01256598565
+                <p>{phone}</p>
                 <span
                   onClick={() => setEditPhone(true)}
                   className="font-medium text-indigo-600 hover:text-indigo-500 mr-32 cursor-pointer ml-5"
@@ -154,6 +196,8 @@ const MyProfile = () => {
               </dd>
             )}
           </div>
+
+          {/* LinkedIn profile */}
           <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">
               LinkedIn profile
@@ -161,13 +205,17 @@ const MyProfile = () => {
             {editLinkedIn ? (
               <dd className="lg:flex lg:gap-2">
                 <input
+                  onChange={(e) => setUpdata(e.target.value)}
                   type="text"
-                  id="simple-email"
                   class="   border border-t-0 border-x-0 border-gray-300 w-full pt-px px-4  bg-white text-gray-700 placeholder-gray-400  text-base focus:outline-none  "
                   placeholder="Your LinkedIn profile"
                 />
                 <span
-                  href="#"
+                  onClick={(e) => {
+                    handleUpdate(e.target.id);
+                    setEditLinkedIn(false);
+                  }}
+                  id="linkedIn"
                   className="font-medium text-sm text-indigo-600 hover:text-indigo-500 cursor-pointer"
                 >
                   Update
@@ -182,7 +230,7 @@ const MyProfile = () => {
               </dd>
             ) : (
               <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex justify-between">
-                https://www.linkedin.com/
+                <p>{linkedIn} </p>
                 <span
                   onClick={() => setEditLinkedIn(true)}
                   className="font-medium text-indigo-600 hover:text-indigo-500 mr-32 cursor-pointer ml-5"
