1f8bbc64b2e10e439f5e7dcc12253f7cc639c9b0
Sakib Rahman <seu.sakib@gmail.com>
2022-05-26 19:45:40 +0600

    add blog question

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -15,7 +15,18 @@ const Blogs = () => {
             </h3>
           </dt>
           <dd class="mb-16">
-            <p>Ans 1</p>
+            <p>
+              There are several way to improve the performance of a react
+              application. Some of them:
+              <ul>
+                <li>Use Production build. </li>
+                <li>
+                  React show us many warning. By removing them can optimize.
+                </li>
+                <li>By using conditional rendering.</li>
+                <li>Using Caching function.</li>
+              </ul>
+            </p>
           </dd>
           <dt class="mb-4">
             <h3 class="text-xl font-semibold">
@@ -24,7 +35,13 @@ const Blogs = () => {
             </h3>
           </dt>
           <dd class="mb-16">
-            <p>Ans 2</p>
+            <p>
+              There are many possible way to manage state in react. one is
+              useState hook. First we have to import useState hook and then
+              initialize the state. we can also manage state with react context
+              which is help you to avoid pop drilling. We can aloso manage state
+              with reactQuery which is also has caching thing.
+            </p>
           </dd>
           <dt class="mb-4">
             <h3 class="text-xl font-semibold">
@@ -32,7 +49,13 @@ const Blogs = () => {
             </h3>
           </dt>
           <dd class="mb-16">
-            <p>Ans 3</p>
+            <p>
+              By prototypical inheritance, it is possible to access other object
+              properties and methods. <br /> ChildObject.__proto__ =
+              ParentObject their is two object ChildObject and ParentObject. by
+              applying prototypical inheritance ChildObject can now access
+              ParentObject properties and methods.
+            </p>
           </dd>
           <dt class="mb-4">
             <h3 class="text-xl font-semibold">
@@ -42,7 +65,13 @@ const Blogs = () => {
             </h3>
           </dt>
           <dd class="mb-16">
-            <p>Ans 4</p>
+            <p>
+              For state management we use useState. When we use useState. it
+              track the state. When the state get update. It tell react that
+              this component and its children needs to re-rendered.Then react
+              re-rendered it. If we do it directly. React can't track it. Cant
+              re-rendered properly.
+            </p>
           </dd>
           <dt class="mb-4">
             <h3 class="text-xl font-semibold">
@@ -52,7 +81,12 @@ const Blogs = () => {
             </h3>
           </dt>
           <dd class="mb-16">
-            <p>Ans 5</p>
+            <p>
+              To find products by name will use products.find when it found
+              object it will return that product. Code: products . find ( e = >
+              e . name === ' searched _ name ' ) when it found the object it
+              will return the obj.
+            </p>
           </dd>
           <dt class="mb-4">
             <h3 class="text-xl font-semibold">
@@ -60,7 +94,13 @@ const Blogs = () => {
             </h3>
           </dt>
           <dd class="mb-16">
-            <p>Ans 6</p>
+            <p>
+              Unite test is a short function that test a small unit of
+              production code's behavior. And it producing pass or fail result.
+              By using unite testing developer can catch bugs before go to
+              production. It also help to verify internal design and internal
+              logic.
+            </p>
           </dd>
         </dl>
       </div>
